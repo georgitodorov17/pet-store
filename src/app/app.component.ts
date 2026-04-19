755a7dc4abc7f82d5cac47123a224648ccb33688
stu2301681041 <stu2301681041@uni-plovdiv.bg>
2026-04-19 14:50:25 +0300

    dynamic navigation based on authentication state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { Observable } from 'rxjs';
+import { User as FirebaseUser } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pet-store';
+
+  user$: Observable<FirebaseUser | null>;
+
+  constructor(private authService: AuthService) {
+    this.user$ = this.authService.user$;
+  }
+
+  logout(): void {
+    this.authService.logout()
+      .catch(err => console.error(err));
+  }
 }
