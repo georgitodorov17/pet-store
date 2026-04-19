5990985e575a5b7b95b44cb2a7f306f58b88f3d0
stu2301681041 <stu2301681041@uni-plovdiv.bg>
2026-04-19 14:27:09 +0300

    auth service with firebase authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,15 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"pet-store-9b052","appId":"1:1002969709907:web:6f72457c1f10ab1a9bf5a6","storageBucket":"pet-store-9b052.firebasestorage.app","apiKey":"AIzaSyBg8nVG1S_T0jfMWcE2S_Jcsw2mFXLwFuE","authDomain":"pet-store-9b052.firebaseapp.com","messagingSenderId":"1002969709907","measurementId":"G-EWNH9ZED9S","projectNumber":"1002969709907","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({
+    "projectId":"pet-store-9b052",
+    "appId":"1:1002969709907:web:6f72457c1f10ab1a9bf5a6",
+    "storageBucket":"pet-store-9b052.firebasestorage.app",
+    "apiKey":"AIzaSyBg8nVG1S_T0jfMWcE2S_Jcsw2mFXLwFuE",
+    "authDomain":"pet-store-9b052.firebaseapp.com",
+    "messagingSenderId":"1002969709907",
+    "measurementId":"G-EWNH9ZED9S"
+    //"projectNumber":"1002969709907",
+    //"version":"2"
+  })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, user } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor() {}
+
+  user$: Observable<any>;
+
+  constructor(private auth: Auth) {
+    this.user$ = user(this.auth);
+  }
+
+  register(email: string, password: string) {
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  login(email: string, password: string) {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  logout() {
+    return signOut(this.auth);
+  }
 }
