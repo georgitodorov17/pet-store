c5b004bbc5e412a4e91bda6fea49bf7872dd87c7
stu2301681041 <stu2301681041@uni-plovdiv.bg>
2026-04-20 14:59:50 +0300

    details page with route param and ownership logic

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { PetService } from '../../services/pet.service';
+import { Pet } from '../../models/pet.model';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
 
+  pet: Pet | null = null;
+  isOwner: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private petService: PetService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    const id = this.route.snapshot.paramMap.get('id')!;
+
+    this.pet = await this.petService.getPetById(id);
+
+    this.authService.user$.subscribe(user => {
+      if (user && this.pet) {
+        this.isOwner = user.uid === this.pet.ownerId;
+      }
+    });
+  }
+
+  async deletePet() {
+    if (!this.pet?.id) return;
+
+    await this.petService.deletePet(this.pet.id);
+    this.router.navigate(['/catalog']);
+  }
 }
