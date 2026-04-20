47b8873d2ba41bf9beae0c05f6f18fd521b59e3f
stu2301681041 <stu2301681041@uni-plovdiv.bg>
2026-04-20 14:58:07 +0300

    catalog page with observable pet list

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PetService } from '../../services/pet.service';
+import { Pet } from '../../models/pet.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './catalog.component.html',
-  styleUrl: './catalog.component.css'
+  styleUrls: ['./catalog.component.css']
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
 
+  pets$!: Observable<Pet[]>;
+
+  constructor(private petService: PetService) {}
+
+  ngOnInit(): void {
+    this.pets$ = this.petService.getPets();
+  }
 }
--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@angular/core';
+import {Firestore,collection,collectionData,addDoc,doc,deleteDoc,updateDoc,getDoc} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Pet } from '../models/pet.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PetService {
-  constructor() {}
+
+  private petsCollection;
+
+  constructor(private firestore: Firestore) {
+    this.petsCollection = collection(this.firestore, 'pets');
+  }
+
+  getPets(): Observable<Pet[]> {
+    return collectionData(this.petsCollection, { idField: 'id' }) as Observable<Pet[]>;
+  }
+
+  async getPetById(id: string): Promise<Pet | null> {
+    const petDocRef = doc(this.firestore, `pets/${id}`);
+    const snapshot = await getDoc(petDocRef);
+
+    if (snapshot.exists()) {
+      return { id: snapshot.id, ...(snapshot.data() as Pet) };
+    }
+
+    return null;
+  }
+
+ 
+  addPet(pet: Pet) {
+    return addDoc(this.petsCollection, pet);
+  }
+
+ 
+  updatePet(id: string, pet: Partial<Pet>) {
+    const petDocRef = doc(this.firestore, `pets/${id}`);
+    return updateDoc(petDocRef, pet);
+  }
+
+
+  deletePet(id: string) {
+    const petDocRef = doc(this.firestore, `pets/${id}`);
+    return deleteDoc(petDocRef);
+  }
 }
